f3363206747a9839775cac876a4a199bdd098f55
Kencrazy <lienquanaren@gmail.com>
2024-09-23 10:09:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ function loadLabeledImages() {
     labels.map(async label => {
       const descriptions = []
       for (let i = 1; i <= 2; i++) {
-        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/Kencrazy/face/main/labeled_images/${label}/${i}.jpg`)
+        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/Kencrazy/final/main/labeled_images/${label}/${i}.jpg`)
         const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
         descriptions.push(detections.descriptor)
       }
